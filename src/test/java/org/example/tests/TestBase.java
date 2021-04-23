15e505d8deb381b6495524b674d03b41bc941019
MarinaQA2016 <marinaqatest2016@gmail.com>
2021-04-23 11:55:19 +0300

    ClassWork EventFiringWebDriver, ScreenShots, ITestResult

--- src/test/java/org/example/tests/TestBase.java
+++ src/test/java/org/example/tests/TestBase.java
@@ -1,15 +1,19 @@
 package org.example.tests;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import com.google.common.io.Files;
 import org.example.SuiteConfiguration;
 import org.example.pages.HomePageHelper;
 import org.example.util.LogLog4j;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.*;
 
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -27,10 +31,42 @@ public class TestBase {
   protected static Capabilities capabilities;
   public static LogLog4j log4j = new LogLog4j();
 
-  protected WebDriver driver;
+  //protected WebDriver driver;
+  protected EventFiringWebDriver driver;
   public HomePageHelper homePage;
   public static final String LOGIN = "marinaqatest2019@gmail.com";
   public static final String PASSWORD = "marinaqa";
+
+  public static class MyListener extends AbstractWebDriverEventListener{
+    @Override
+    public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+      log4j.info("Find element: " + by);
+    }
+
+    @Override
+    public void afterFindBy(By by, WebElement element, WebDriver driver) {
+      log4j.info(by + " element was found");
+    }
+
+    @Override
+    public void onException(Throwable throwable, WebDriver driver) {
+      log4j.error("Error: " + throwable);
+      getScreenShot((TakesScreenshot) driver);
+    }
+  }
+
+  private static void getScreenShot(TakesScreenshot driver) {
+    File tmp = driver.getScreenshotAs(OutputType.FILE);
+    File screen = new File("screen-"+System.currentTimeMillis()+ ".png");
+    try {
+      Files.copy(tmp,screen);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    log4j.info("============================");
+    log4j.info("See screen: " + screen);
+    log4j.info("============================");
+  }
 
 
   @BeforeSuite
@@ -46,10 +82,20 @@ public class TestBase {
   @BeforeMethod
   public void initWebDriver() {
 
-    driver = WebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities);
+    driver = new EventFiringWebDriver(WebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities));
+    driver.register(new MyListener());
     homePage = PageFactory.initElements(driver,HomePageHelper.class);
     driver.get(baseUrl);
     homePage.waitUntilPageIsLoaded();
+  }
+  @AfterMethod
+  public void finishTest(ITestResult result){
+    if(result.getStatus()==ITestResult.FAILURE){
+      log4j.info("!!!!!!!!!!!!!!!!!!!!!!!!");
+      log4j.info("Test failed");
+      log4j.info("!!!!!!!!!!!!!!!!!!!!!!!!");
+      getScreenShot((TakesScreenshot) driver);
+    }
   }
 
   @AfterMethod(alwaysRun = true)
